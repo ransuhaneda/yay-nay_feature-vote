c3517727adf951542602616807887115611bf365
ransu <185787867+ransuhaneda@users.noreply.github.com>
2025-11-27 06:33:34 +0800

    feat: roles & permission config  - create Enums for Roles and Permissions  - assign user roles Admin, Commenter, & User  - create a DB seeder applying roles and permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Enum\PermissionsEnum;
+use App\Enum\RolesEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,15 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+
+        $userRole = Role::create(['name' => RolesEnum::User->value]);
+        $commenterRole = Role::create(['name' => RolesEnum::Commenter->value]);
+        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+
+        $manageFeaturesPermission = Permission::create(['name' => PermissionsEnum::ManageFeatures->value]);
+        $manageCommentsPermission = Permission::create(['name' => PermissionsEnum::ManageComments->value]);
+        $manageUsersPermission = Permission::create(['name' => PermissionsEnum::ManageUsers->value]);
+        $upvoteDownvotePermission = Permission::create(['name' => PermissionsEnum::UpvoteDownvote->value]);
+
+        $userRole->syncPermissions([$upvoteDownvotePermission]);
+        $commenterRole->syncPermissions([$upvoteDownvotePermission, $manageCommentsPermission]);
+        $adminRole->syncPermissions([$upvoteDownvotePermission, $manageCommentsPermission, $manageUsersPermission, $manageFeaturesPermission]);
 
         User::firstOrCreate(
-            ['email' => 'test@example.com'],
+            ['email' => 'user@example.com'],
             [
-                'name' => 'Test User',
+                'name' => 'Normal User',
                 'password' => 'password',
                 'email_verified_at' => now(),
             ]
-        );
+        )->assignRole(RolesEnum::User);
+        User::firstOrCreate(
+            ['email' => 'commenter@example.com'],
+            [
+                'name' => 'Commenter User',
+                'password' => 'password',
+                'email_verified_at' => now(),
+            ]
+        )->assignRole(RolesEnum::Commenter);
+        User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => 'password',
+                'email_verified_at' => now(),
+            ]
+        )->assignRole(RolesEnum::Admin);
     }
 }
