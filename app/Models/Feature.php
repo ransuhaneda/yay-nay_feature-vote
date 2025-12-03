d748029b2c60fbdf24f8c1bda8a4aae621e5c078
ransu <185787867+ransuhaneda@users.noreply.github.com>
2025-12-03 17:50:25 +0800

    feat: define relationships and db schema

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Feature extends Model
 {
-    //
+    public function upvotes(): HasMany
+    {
+      return $this->HasMany(Upvote::class);
+    }
+
+    public function comments(): HasMany
+    {
+      return $this->HasMany(Comment::class);
+    }
+
+    public function user(): BelongsTo
+    {
+      return $this->BelongsTo(User::class);
+    }
 }
