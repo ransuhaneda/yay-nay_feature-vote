6de0d9bf9fe49a6a4572a3d401236fc3a69a14d6
ransu <185787867+ransuhaneda@users.noreply.github.com>
2025-12-16 21:47:21 +0800

    feat: prepare CRUD operations  - CRUD routes using resource  - define CRUD operations on controller  - create pages in frontend

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-    Route::get('features', [FeatureController::class, 'index'
-    ])->name('feature.index');
+
+
+    Route::resource('features', FeatureController::class);
 });
 
 require __DIR__.'/settings.php';
